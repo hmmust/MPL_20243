1a04b37950c23218213fe385e3d1a4b23e9f74ed
hossam <hossa@uop.edu.jo>
2025-07-22 10:55:34 +0300

    part1

---
+++ lect1.js
@@ -0,0 +1,6 @@
+Name = "Ahmad";
+var Name= "Ahmad";
+let Name2= "Ahmad"; // unique identifier
+const Name3= "Ahmad";// unique identifier
+//Name3 = "Mohammad";
+console.log(Name);
---
+++ lect1b.js
@@ -0,0 +1,17 @@
+let Name = "Ahmad";
+let age = 20;
+let age2 = 5;
+let age3= age**age2; // ** / .. 
+let age4 = null;
+let age5 = undefined;
+console.log(age3);
+console.log(Name*age2);
+console.log(Name+age2);
+console.log(age+age4);
+console.log(age+age5);
+console.log(age>age4);
+console.log(age!=age5);
+console.log(age>age5);
+
+
+
