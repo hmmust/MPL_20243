82522e1ff0c02f12c429dd0bdd00a8db2b2763d2
hossam <hossa@uop.edu.jo>
2025-08-03 11:51:18 +0300

    shop

--- shop/app.js
+++ shop/app.js
@@ -2,11 +2,13 @@ const express=require('express');
 const app=express();
 app.set('view engine','ejs');
 app.set('views','views');
+//const mongoose= require('mongoose');
 const bodyParser=require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/",(req,res)=>{
-res.send("<h1>First Application</h1>")
-});
+app.get("/search",(req,res)=>{
+    res.write('<h1>Hossam</h1>');
+    res.end("<h1>First Application</h1>")
+    });
 app.listen(8080);
